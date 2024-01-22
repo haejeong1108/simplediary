4c7df25df85ae31ac03f383decb4e936504e77fa
haejung1108 <h.hjeong1108@gmail.com>
2024-01-22 23:13:23 +0900

    データ修正

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,11 +1,43 @@
+import { useRef, useState } from "react";
+
 const DiaryItem = ({
-  onDelete,
+  onEdit,
+  onRemove,
   author,
   content,
   created_date,
   emotion,
   id,
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput = useRef();
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}番目の日記を削除しますか。`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handelEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}番目の日記を修正しますか。`)) {
+      onEdit(id, localContent);
+      toggleEdit();
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -15,16 +47,31 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}番目の日記を削除しますか。`)) {
-            onDelete(id);
-          }
-        }}
-      >
-        削除
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>取消</button>
+          <button onClick={handelEdit}>登録</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>削除</button>
+          <button onClick={toggleEdit}>編集</button>
+        </>
+      )}
     </div>
   );
 };
